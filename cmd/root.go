493446c887e095ebad296bdf1714e962bc49ce5f
shangfengli <shangfengli@tencent.com>
2022-12-09 12:20:58 +0800

    add handler

--- cmd/root.go
+++ cmd/root.go
@@ -1,11 +1,15 @@
 package cmd
 
 import (
+	"net/http"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
 	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/klog/v2"
+
+	"github.com/librant/kube-console/handler"
 )
 
 // kubeConfig kube-config path
@@ -17,6 +21,14 @@ var rootCmd = &cobra.Command{
 	Short: "kube-console for multi-cluster container login",
 	Run: func(cmd *cobra.Command, args []string) {
 		klog.Infof("kube-console run begin args: %v", args)
+
+		router := gin.Default()
+		// 静态资源加载，本例为 css,js 以及资源图片
+		router.StaticFS("/static/", http.Dir("."))
+		router.StaticFile("/favicon.ico", "/static/favicon.ico")
+
+		router.GET("/login", handler.IndexHandler)
+		klog.Fatal(router.Run(":8080"))
 	},
 }
 
---
+++ handler/handler.go
@@ -0,0 +1,10 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// IndexHandler index handler
+func IndexHandler(c *gin.Context) {
+
+}
